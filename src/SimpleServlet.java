71d8f6b61f3df457cbf70d0d11b07dbdbf5a1681
Vishesh <visheshsingh16@gmail.com>
2017-12-16 20:10:56 -0500

    Servlet implemented using Tomcat Server

--- src/SimpleServlet.java
+++ src/SimpleServlet.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,12 +16,11 @@ import javax.servlet.http.HttpServletResponse;
 public class SimpleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		PrintWriter pw = response.getWriter();
+		System.out.println("Hello from GET method!");
+		pw.println("<h2>hello</h2>");
 	}
 
 }
