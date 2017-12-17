ce7b2febeadaadb4267213e592d8e4ce1541db25
Vishesh <visheshsingh16@gmail.com>
2017-12-16 20:44:48 -0500

    Servlet XML Configuration

---
+++ src/XmlServlet.java
@@ -0,0 +1,25 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/XmlServlet")
+public class XmlServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		
+		pw.println("Response from XML Servlet!");
+		System.out.println("XML response rendered!");
+		
+	}
+
+}
