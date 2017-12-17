aa14601b0c9792fe16a17c8cddf78468ce3f87e7
Vishesh <visheshsingh16@gmail.com>
2017-12-17 17:08:53 -0500

    Sessions

---
+++ src/SecondServlet.java
@@ -0,0 +1,26 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/SecondServlet")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		String str = session.getAttribute("userfirst").toString();
+		PrintWriter pw = response.getWriter();
+		
+		pw.println("In second servlet" +  str);
+		
+	}
+}
